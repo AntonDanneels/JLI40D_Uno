638ac02dc1620aa3be22fc2216d593046a71f7da
Pieter Delobelle <pieter.delobelle@gmail.com>
2017-10-19 15:44:42 +0200

    Created main

---
+++ src/main/java/be/kuleuven/cs/jli40d/Main.java
@@ -0,0 +1,8 @@
+package be.kuleuven.cs.jli40d;
+
+/**
+ * @author Pieter
+ * @version 1.0
+ */
+public class Main {
+}
---
+++ src/test/java/be/kuleuven/cs/jli40d/MainTest.java
@@ -0,0 +1,9 @@
+package be.kuleuven.cs.jli40d;
+
+/**
+ * @author Pieter
+ * @version 1.0
+ */
+public class MainTest {
+
+}
