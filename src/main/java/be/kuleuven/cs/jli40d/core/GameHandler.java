2f9714d4e23fe506c118d59d2dff262a55890a9b
AntonDanneels <antondanneels@hotmail.com>
2017-10-19 20:15:14 +0200

    Added GameMove class

--- src/main/java/be/kuleuven/cs/jli40d/core/GameHandler.java
+++ src/main/java/be/kuleuven/cs/jli40d/core/GameHandler.java
@@ -1,6 +1,7 @@
 package be.kuleuven.cs.jli40d.core;
 
 import be.kuleuven.cs.jli40d.core.model.Card;
+import be.kuleuven.cs.jli40d.core.model.GameMove;
 
 /**
  * Created by Anton D. on 19/10/2017 using IntelliJ IDEA 14.0
@@ -8,6 +9,6 @@ import be.kuleuven.cs.jli40d.core.model.Card;
 public interface GameHandler
 {
     boolean isStarted();
-    Card getNextMove();
-    void sendMove();
+    GameMove getNextMove();
+    void sendMove( GameMove move );
 }
---
+++ src/main/java/be/kuleuven/cs/jli40d/core/model/GameMove.java
@@ -0,0 +1,11 @@
+package be.kuleuven.cs.jli40d.core.model;
+
+/**
+ * Created by Anton D.
+ */
+public class GameMove
+{
+    private Player player;
+    private Card playedCard;
+    boolean cardDrawn;
+}
