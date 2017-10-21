5457253d84968d8bf5757dcc24069d0e3595c819
AntonDanneels <antondanneels@hotmail.com>
2017-10-21 19:28:48 +0200

    Added mock UI for testing

--- src/main/java/be/kuleuven/cs/jli40d/client/Client.java
+++ src/main/java/be/kuleuven/cs/jli40d/client/Client.java
@@ -7,6 +7,13 @@ import be.kuleuven.cs.jli40d.core.model.Game;
 import be.kuleuven.cs.jli40d.core.model.GameMove;
 import be.kuleuven.cs.jli40d.core.model.Player;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Anton D. on 19/10/2017 using IntelliJ IDEA 14.0
  */
@@ -48,8 +55,87 @@ public class Client
 
     public static void main( String args[] )
     {
-        Client client = new Client();
-        client.run();
+        //Client client = new Client();
+        //client.run();
+
+        try
+        {
+            UIManager.setLookAndFeel( UIManager.getSystemLookAndFeelClassName() );
+        }
+        catch ( Exception e )
+        {
+        }
+
+        final JFrame frame = new JFrame( "Uno" );
+        frame.setSize( 900, 600 );
+        frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        frame.setResizable( false );
+
+        final JPanel loginPanel = new JPanel();
+
+        JTextField usernameField = new JTextField( "Username" );
+        JPasswordField passwordField = new JPasswordField( "password" );
+        JButton login = new JButton( "Login" );
+        JButton register = new JButton( "Register" );
+
+        register.addActionListener( new ActionListener()
+        {
+            public void actionPerformed( ActionEvent e )
+            {
+                JPanel lobbyListPanel = new JPanel();
+
+                // fetch list here
+                List<Game> games = new ArrayList<Game>();
+                games.add( new Game() );
+
+                lobbyListPanel.setLayout( new GridLayout( games.size(), 1 ) );
+
+                for( int i = 0; i < games.size(); i++ )
+                {
+                    Game game = games.get( i );
+                    JPanel panel = new JPanel();
+                    panel.add( new JLabel( "Game " + i ) );
+                    panel.add( new JLabel( "Players: " + game.getPlayers().size()  ) );
+
+                    JButton join = new JButton( "Join" );
+                    JButton spectate = new JButton( "View" );
+
+                    join.addActionListener( new ActionListener()
+                    {
+                        public void actionPerformed( ActionEvent e )
+                        {
+                            // TODO Join game here
+                        }
+                    } );
+
+                    spectate.addActionListener( new ActionListener()
+                    {
+                        public void actionPerformed( ActionEvent e )
+                        {
+                            // TODO Spectate game here
+                        }
+                    } );
+
+                    panel.add( join );
+                    panel.add( spectate );
+
+                    lobbyListPanel.add( panel );
+                }
+
+                frame.remove( loginPanel );
+                frame.add( lobbyListPanel );
+                frame.validate();
+            }
+        } );
+
+        loginPanel.add( usernameField );
+        loginPanel.add( passwordField );
+        loginPanel.add( register );
+        loginPanel.add( login );
+
+        frame.add( loginPanel );
+        frame.setVisible( true );
+
     }
 
 }
