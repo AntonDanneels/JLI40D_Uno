a81d91334fb0a4b118f2761e85efaa0a619b9756
Pieter Delobelle <pieter.delobelle@gmail.com>
2017-10-21 22:15:02 +0200

    Added RemoteExceptions to GameHandler functions

--- src/main/java/be/kuleuven/cs/jli40d/core/GameHandler.java
+++ src/main/java/be/kuleuven/cs/jli40d/core/GameHandler.java
@@ -3,17 +3,18 @@ package be.kuleuven.cs.jli40d.core;
 import be.kuleuven.cs.jli40d.core.model.GameMove;
 
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 
 /**
  * All methods expect a token to identify and authenticate the user.
  */
 public interface GameHandler extends Remote
 {
-    boolean isStarted( String token );
+    boolean isStarted( String token ) throws RemoteException;
 
-    boolean myTurn( String token );
+    boolean myTurn( String token ) throws RemoteException;
 
-    GameMove getNextMove( String token, int id );
+    GameMove getNextMove( String token, int id ) throws RemoteException;
 
-    void sendMove( String token, GameMove move );
+    void sendMove( String token, GameMove move ) throws RemoteException;
 }
