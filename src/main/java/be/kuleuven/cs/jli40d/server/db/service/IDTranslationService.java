b676ca193240221f3078b89f2a209bec4ef32949
Pieter Delobelle <pieter.delobelle@gmail.com>
2017-11-06 15:58:00 +0100

    Finished DatabaseGameService and IDTranslationService should work ...

--- src/main/java/be/kuleuven/cs/jli40d/server/db/service/IDTranslationService.java
+++ src/main/java/be/kuleuven/cs/jli40d/server/db/service/IDTranslationService.java
@@ -5,6 +5,9 @@ import be.kuleuven.cs.jli40d.core.model.GameMove;
 import be.kuleuven.cs.jli40d.core.model.Player;
 import be.kuleuven.cs.jli40d.core.model.exception.GameNotFoundException;
 import be.kuleuven.cs.jli40d.server.db.model.GameMapping;
+import javafx.util.Pair;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
@@ -21,7 +24,11 @@ import java.util.Map;
 @Service
 public class IDTranslationService
 {
+    private static final Logger LOGGER = LoggerFactory.getLogger( IDTranslationService.class );
+
     private Map<Integer, Map<Integer, GameMapping>> gameMappingsForServers = new HashMap<>();
+
+    private Map<Integer, GameMapping> DBGameIDMappings = new HashMap<>();
 
     public IDTranslationService()
     {
@@ -35,19 +42,24 @@ public class IDTranslationService
      * @return
      * @throws GameNotFoundException
      */
-    public int translateToGameID( int serverID, int gameID ) throws GameNotFoundException
+    public int translateToGameID( int serverID, int gameID )
     {
         return gameMappingsForServers.get( serverID ).get( gameID ).getGameIDOnDB();
     }
 
-    public int translateToGameMoveID( int serverID, int gameID, int gameMoveID ) throws GameNotFoundException
+    public int translateToGameMoveID( int serverID, int gameID, int gameMoveID )
     {
         return gameMappingsForServers.get( serverID ).get( gameID ).getGameMoveID( gameMoveID );
     }
 
-    public int translateToPlayerID( int serverID, int gameID, int playerID ) throws GameNotFoundException
+    public int translateToPlayerID( int serverID, int gameID, int playerID )
     {
         return gameMappingsForServers.get( serverID ).get( gameID ).getPlayerID( playerID );
+    }
+
+    public boolean containsGameID( int serverID, int gameID )
+    {
+        return gameMappingsForServers.containsKey( serverID ) && gameMappingsForServers.get( serverID ).containsKey( gameID );
     }
 
     public void addGame( int serverID, int gameID, int gameIDOnDB )
@@ -58,17 +70,58 @@ public class IDTranslationService
             gameMappingsForServers.put( serverID, new HashMap<>() );
         }
 
-        gameMappingsForServers.get( serverID ).put( gameID, new GameMapping( serverID, gameID, gameIDOnDB ) );
+        GameMapping gameMapping = new GameMapping( serverID, gameID, gameIDOnDB );
+
+        gameMappingsForServers.get( serverID ).put( gameID, gameMapping );
+
+        DBGameIDMappings.put( gameIDOnDB, gameMapping );
+
     }
 
     public void addGameMoveID( int serverID, int gameID, int gameMoveID, int gameMoveIDOnDB )
     {
+        LOGGER.debug( "Added gamemove id: server = {}, game = {}, gameMove = {} -> {}",
+                serverID,
+                gameID,
+                gameMoveID,
+                gameMoveIDOnDB );
+
         gameMappingsForServers.get( serverID ).get( gameID ).addGameMoveID( gameMoveID, gameMoveIDOnDB );
     }
 
     public void addPlayerID( int serverID, int gameID, int playerID, int playerIDOnDB )
     {
+        LOGGER.debug( "Added player id: server = {}, game = {}, player = {} -> {}",
+                serverID,
+                gameID,
+                playerID,
+                playerIDOnDB );
+
         gameMappingsForServers.get( serverID ).get( gameID ).addGameMoveID( playerID, playerIDOnDB );
+    }
+
+    public int addServer()
+    {
+        int serverID = gameMappingsForServers.size();
+
+        gameMappingsForServers.put( serverID, new HashMap<>() );
+
+        return serverID;
+    }
+
+    /**
+     * Returns a {@link Pair} pair with K (the first) as the server id and
+     * V (second) as the game ID.
+     *
+     * @param gameIDOnDB
+     * @return
+     */
+    public Pair<Integer, Integer> translateFromGameID( int gameIDOnDB )
+    {
+        GameMapping gameMapping = DBGameIDMappings.get( gameIDOnDB );
+
+        return new Pair<>( gameMapping.getServerID(), gameMapping.getGameIDOnServer() );
+
     }
 
 }
