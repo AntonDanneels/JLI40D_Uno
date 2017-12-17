0ef485a00cc4b1f080aae17993300a90efe46915
Pieter Delobelle <pieter.delobelle@gmail.com>
2017-12-17 14:45:12 +0100

    Added api for commits

---
+++ src/main/java/be/kuleuven/cs/jli40d/server/db/service/NotPreparedException.java
@@ -0,0 +1,30 @@
+package be.kuleuven.cs.jli40d.server.db.service;
+
+/**
+ * @author Pieter
+ * @version 1.0
+ */
+public class NotPreparedException extends Exception
+{
+    /**
+     * Constructs a new exception with {@code null} as its detail message.
+     * The cause is not initialized, and may subsequently be initialized by a
+     * call to {@link #initCause}.
+     */
+    public NotPreparedException()
+    {
+    }
+
+    /**
+     * Constructs a new exception with the specified detail message.  The
+     * cause is not initialized, and may subsequently be initialized by
+     * a call to {@link #initCause}.
+     *
+     * @param message the detail message. The detail message is saved for
+     *                later retrieval by the {@link #getMessage()} method.
+     */
+    public NotPreparedException( String message )
+    {
+        super( message );
+    }
+}
